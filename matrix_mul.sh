6c03fcfba2c3bad62215e576489d22e44ac8e22c
Ronil Biswas <66785801+RonBis@users.noreply.github.com>
2024-01-05 06:19:52 +0000

    added muatrix_mul and wc

--- matrix_mul.sh
+++ matrix_mul.sh
@@ -8,29 +8,59 @@ read p
 echo -n "Number of columns for matrix B: "
 read q
 
+
+
 if [ $n -ne $p ]; then # checking compatibility of the two matrices for multiplication
-echo "Wrong input! Multiplication is not possible"
+	echo "Wrong input! Multiplication is not possible"
 else
-echo "Enter elements of matrix A"
-for (( i=0;i<m;i++ )); do
-    for (( j=0;j<q;n++ )); do
-        read a[$i$j]
-    done
-done
+	echo "Enter elements of matrix A"
+	for (( i=0;i<m;i++ )); do
+    	for (( j=0;j<n;j++ )); do
+        	read a[$i$j]
+    	done
+	done
 
-echo "Enter elements of matrix A"
-for (( i=0;i<m;i++ )); do
-    for (( j=0;j<q;n++ )); do
-        read a[$i$j]
-    done
-done
+	echo "Enter elements of matrix B"
+	for (( i=0;i<p;i++ )); do
+	    for (( j=0;j<q;j++ )); do
+	        read b[$i$j]
+	    done
+	done
 
-# matrix 1
-for (( i=0;i<m;i++ )); do
-    for (( j=0;j<q;n++ )); do
-        echo a[$i$j]
-    done
-    echo "\n"
-done
+	# matrix 1
+	echo "Matrix A"
+	for (( i=0;i<m;i++ )); do
+    	for (( j=0;j<n;j++ )); do
+    	    echo -n "${a[$i$j]}  "
+    	done
+    	echo
+	done
+
+	# matrix 2
+	echo "Matrix B"
+	for (( i=0;i<p;i++ )); do
+    	for (( j=0;j<q;j++ )); do
+    	    echo -n "${b[$i$j]}  "
+    	done
+    	echo
+	done
+
+	for (( i=0;i<m;i++ )); do
+		for (( j=0;j<q;j++ )); do
+			mul[$i$j]=0
+			for (( k=0;k<p;k++ )); do
+				mul[$i$j]=`expr ${mul[$i$j]} + ${a[$i$k]} \* ${b[$k$j]}`
+			done
+		done
+	done
+
+	# output matrix
+	echo "Product matrix"
+	for (( i=0;i<m;i++ )); do
+    	for (( j=0;j<q;j++ )); do
+    	    echo -n "${mul[$i$j]}  "
+    	done
+    	echo
+	done
 
 fi
---
+++ mywc.sh
@@ -0,0 +1,37 @@
+# simulate the wc command (word count)
+
+# $1 -> flag
+# $2 -> source file
+
+exec<$2
+
+case $1 in
+"-l")
+    nol=0
+    while read line
+    do
+        nol=`expr $nol + 1`
+    done
+    echo $nol;;
+"-w")
+    now=0
+    while read line
+    do
+        set $line
+        now=`expr $now + $#` # no of args(words) in each line
+    done
+    echo $now;;
+"-c")
+    noc=0
+    while read line
+    do
+        set $line
+        for word in $@  # '$@ means all args(words) in each line'
+        do
+            noc=`expr $noc + length $word`
+        done
+    done
+    echo $noc;;
+*)
+    echo "Invalid argument"
+esac
